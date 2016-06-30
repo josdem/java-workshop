f938436fde7d0535f5bb76cce1b8e7f33cfb90d5
josdem <joseluis.delacruz@gmail.com>
2016-06-30 15:28:03 -0500

    Adding class sorter

---
+++ QuickSorter.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class QuickSorter{
+  public List<Integer> sort(List<Integer> data){
+    return data;
+  }
+}
+
+
--- Sorter.java
+++ Sorter.java
@@ -3,15 +3,20 @@ import java.util.Arrays;
 
 public class Sorter {
 
-  private void list(){
-    List<Integer> data = Arrays.asList(7,3,5,1,8,4,2,0,6,9);
+  private List<Integer> sort(List<Integer> data){
+    QuickSorter sorter = new QuickSorter();
+    return sorter.sort(data);
+  }
+
+  private void print(List<Integer> data){
     data.forEach(item ->
       System.out.println(item)
     );
   }
 
   public static void main(String[] args){
-    new Sorter().list();
+    List<Integer> data = Arrays.asList(7,3,5,1,8,4,2,0,6,9);
+    new Sorter().sort(data);
   }
 
 }
