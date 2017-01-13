bf33c60e9e9194d0a6245e9468343862b9ceb032
josdem <joseluis.delacruz@gmail.com>
2017-01-13 15:30:18 -0600

    Adding s3 event example

---
+++ s3-aws-lambda/src/main/java/example/BucketFileTransfer.java
@@ -0,0 +1,17 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+
+public class BucketFileTransfer implements RequestHandler<S3Event, String> {
+
+	@Override
+	public String handleRequest(S3Event event, Context context) {
+		S3EventNotificationRecord record = event.getRecords().get(0);
+		String sourceBucket = record.getS3().getBucket().getName();
+		return sourceBucket;
+	}
+
+}
