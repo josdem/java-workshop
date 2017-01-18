16f69d956c729b4b73a097c24d9fa0bd0a7721d7
josdem <joseluis.delacruz@gmail.com>
2017-01-17 20:08:09 -0600

    Reading key and secret from properties file

--- s3-aws-lambda/src/main/java/example/client/AWSClient.java
+++ s3-aws-lambda/src/main/java/example/client/AWSClient.java
@@ -8,21 +8,30 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 
+import example.utils.ConfigurationLauncher;
+import example.exception.LambdaException;
+
 public class AWSClient {
-  
-  String sourceBucket; 
+
+  String sourceBucket;
   String sourceKey;
   AmazonS3 s3Client;
 
-  public AWSClient(String sourceBucket, String sourceKey){
+  private Configuration getConfig() throws LambdaException {
+    ConfigurationLauncher configurationLauncher = new ConfigurationLauncher();
+    return configurationLauncher.getConfig();
+  }
+
+  public AWSClient(String sourceBucket, String sourceKey) throws LambdaException{
     this.sourceBucket = sourceBucket;
     this.sourceKey = sourceKey;
-    AWSCredentials credentials = new BasicAWSCredentials("Access Key ID", "Secret Access Key");
+    Configuration configuration = getConfig();
+    AWSCredentials credentials = new BasicAWSCredentials(configuration.getString("key.id"), configuration.getString("key.secret"));
     ClientConfiguration clientConfig = new ClientConfiguration();
     this.s3Client = new AmazonS3Client(credentials, clientConfig);
   }
 
-  public S3Object getS3Object() {    
+  public S3Object getS3Object() {
     return getS3Client().getObject(new GetObjectRequest(sourceBucket, sourceKey));
   }
 
