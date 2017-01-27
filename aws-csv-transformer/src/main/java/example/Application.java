5572874b7cfaf5c7d47002f03eae491bc75388f2
josdem <joseluis.delacruz@gmail.com>
2017-01-27 15:29:36 -0600

    #1 Deleting read file action

--- aws-csv-transformer/src/main/java/example/Application.java
+++ aws-csv-transformer/src/main/java/example/Application.java
@@ -10,7 +10,6 @@ public class Application {
   public static void main(String[] args) {
     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     CsvFileReader reader = (CsvFileReader)context.getBean("reader");
-    reader.read("csv.txt");
   }
 
 }
