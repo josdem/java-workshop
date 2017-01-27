bddb45adb4bb136806860b65c06db8c8032dd316
josdem <joseluis.delacruz@gmail.com>
2017-01-27 11:48:08 -0600

    #1 Adding other reader version

--- aws-csv-transformer/src/main/java/example/CsvFileReader.java
+++ aws-csv-transformer/src/main/java/example/CsvFileReader.java
@@ -7,32 +7,27 @@ import java.util.ArrayList;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.commons.csv.CSVParser;
 
 public class CsvFileReader {
 
 	public List<String> read(String path){
     List<String> row = new ArrayList<String>();
     try{
-     FileReader input = new FileReader(path);
-     CSVFormat csvFileFormat = CSVFormat.DEFAULT.withHeader("id", "name", "email");
-     CSVParser csvFileParser = new CSVParser(input, csvFileFormat);
-     List csvRecords = csvFileParser.getRecords(); 
-
-     for (int i = 1; i < csvRecords.size(); i++) {
-      CSVRecord record = (CSVRecord)csvRecords.get(i);
-      String id = record.get("id");
-      String name = record.get("name");
-      String email = record.get("email");
-      row.add(id);
-      row.add(name);
-      row.add(email);
-     }
-  } catch(IOException ioe){
-    System.out.println("Error" + ioe.getMessage());
-  }
-  return row;
-}
+			FileReader input = new FileReader(path);
+			Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader("id", "name", "email").parse(input);
+			for (CSVRecord record : records) {
+				String id = record.get("id");
+				String name = record.get("name");
+				String email = record.get("email");
+        row.add(id);
+        row.add(name);
+        row.add(email);
+      }
+    } catch(IOException ioe){
+      System.out.println("Error" + ioe.getMessage());
+    }
+		return row;
+	}
 
 }
 
