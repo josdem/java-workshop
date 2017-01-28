229b83a3bdfd5f473ff3c6e6168af588ad5da7e3
josdem <joseluis.delacruz@gmail.com>
2017-01-28 07:26:05 -0600

    #1 Completing read csv test

--- aws-csv-transformer/src/test/groovy/example/CsvFileReaderSpec.groovy
+++ aws-csv-transformer/src/test/groovy/example/CsvFileReaderSpec.groovy
@@ -14,6 +14,9 @@ class CsvFileReaderSpec extends Specification {
 		then:
 		result.size() == 4
     result.get(0) == ['id','name','email']
+    result.get(1) == ['1','ahmed','ahmoha@email.com']
+    result.get(2) == ['2','sara','sarasa@email.com']
+    result.get(3) == ['3','josdem','josdem@email.com']
 	}
 
 
