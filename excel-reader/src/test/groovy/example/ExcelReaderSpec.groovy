c5b35f3a6bff8ff6fc19a16ebbc1198cea80aceb
josdem <joseluis.delacruz@gmail.com>
2017-02-08 12:17:29 -0600

    #2 Completing test to get all rows elements

--- excel-reader/src/test/groovy/example/ExcelReaderSpec.groovy
+++ excel-reader/src/test/groovy/example/ExcelReaderSpec.groovy
@@ -15,6 +15,11 @@ class ExcelReaderSpec extends Specification {
       List<String> result = excelReader.readRows(numberOfRows, excelFile)
     then:"We expect to get content"
     result.size() == 5
-	}
-	
+    result.get(0) == 'A'
+    result.get(1) == 'B'
+    result.get(2) == 'C'
+    result.get(3) == 'D'
+    result.get(4) == 'E'
+  }	
+  
 } 
