64a00f63c5bfa88e626641aa1d51192e52bc9d49
josdem <joseluis.delacruz@gmail.com>
2017-02-08 14:07:16 -0600

    #2 Changing the return type to array into array

--- excel-reader/src/main/java/example/ExcelReader.java
+++ excel-reader/src/main/java/example/ExcelReader.java
@@ -1,5 +1,6 @@
 package example;
 
+
 import java.util.List;
 import java.util.ArrayList;
 import java.io.File;
@@ -11,22 +12,30 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelReader {
 
 	List<String> readRows(Integer numberOfRows, File excelFile){
-		List<String> content = new ArrayList<String>();
+		List content = new ArrayList<ArrayList<String>>();
+    List line = new ArrayList<String>();
     try{
       FileInputStream fileInputStream = new FileInputStream(excelFile);
       Workbook workbook = new XSSFWorkbook(fileInputStream);
       Sheet sheet = workbook.getSheetAt(0);
 
       for (Row row : sheet) {
+        line = new ArrayList<String>();
         for (Cell cell : row) {
-          content.add(cell.getStringCellValue());
+          if (cell.getCellTypeEnum() == CellType.STRING) {
+            line.add(cell.getStringCellValue());
+          } else if (cell.getCellTypeEnum() == CellType.NUMERIC) {
+            line.add(String.valueOf(cell.getNumericCellValue()));
+          }
         }
-      }          
+        content.add(line);
+      }
     } catch(FileNotFoundException fne){
       throw new ExcelException(fne.getMessage(), fne);
     } catch(IOException ioe){
--- excel-reader/src/test/groovy/example/ExcelReaderSpec.groovy
+++ excel-reader/src/test/groovy/example/ExcelReaderSpec.groovy
@@ -12,14 +12,13 @@ class ExcelReaderSpec extends Specification {
     and:"An excel file"
       File excelFile = new File("src/test/resources/input.xlsx")
     when:"We read rows"
-      List<String> result = excelReader.readRows(numberOfRows, excelFile)
+      List<List<String>> result = excelReader.readRows(numberOfRows, excelFile)
     then:"We expect to get content"
-    result.size() == 5
-    result.get(0) == 'A'
-    result.get(1) == 'B'
-    result.get(2) == 'C'
-    result.get(3) == 'D'
-    result.get(4) == 'E'
+    result.size() == 4
+    result.get(0) == ['Name', 'Email', 'Ranking']
+    result.get(1) == ['josdem','joseluis.delacruz@gmail.com','5.0']
+    result.get(2) == ['eric','erich@email.com','5.0']
+    result.get(3) == ['martin','martinv@email.com','5.0']
   }	
-  
+
 } 
