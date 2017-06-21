acdf0c1a656350d0da16d8ef077c5e16a82c3b53
josdem <joseluis.delacruz@gmail.com>
2017-06-21 17:33:08 -0500

    Adding count filter example

---
+++ streams/filters/CountFilter.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.function.Predicate;
+
+public class CountFilter {
+
+  private Long parse(){
+    return Arrays.asList("Java", "C++", "Lisp", "Haskell").
+      stream().filter( p -> p.length() == 4).
+      collect(Collectors.counting());
+  }
+
+  public static void main(String[] args){
+    Long result = new CountFilter().parse();
+    assert 2L == result;
+  }
+
+}
