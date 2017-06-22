0d0e14a1a002f6e8c8095d9207abecb25a7a4e6b
josdem <joseluis.delacruz@gmail.com>
2017-06-22 15:42:01 -0500

    Adding grouping by collector example

---
+++ streams/collect/GroupByCollector.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class GroupByCollector {
+
+  private Map<RoleType, List<Person>> parse(List<Person> persons){
+    return persons.stream().collect(Collectors.groupingBy(Person::getType));
+  }
+
+  public static void main(String[] args){
+  	List<Person> persons = Arrays.asList(
+  		new Person("josdem", RoleType.DEVELOPER),
+      new Person("tgtip", RoleType.DEVELOPER),
+      new Person("erich", RoleType.TESTER)
+    );
+    Map<RoleType, List<Person>> result = new GroupByCollector().parse(persons);
+    assert result.get(RoleType.DEVELOPER).size() == 2;
+    assert result.get(RoleType.TESTER).size() == 1;
+  }
+
+}
+
+class Person {
+	String name;
+	RoleType type;
+
+	public Person(String name, RoleType type){
+		this.name = name;
+		this.type = type;
+	}
+
+	RoleType getType(){
+		return type;
+	}
+
+}
+
+enum RoleType {
+	DEVELOPER, TESTER
+}
--- streams/collect/StreamToListConverter.java
+++ streams/collect/StreamToListConverter.java
@@ -6,13 +6,14 @@ public class StreamToListConverter {
 
   private List<String> parse(){
     return Arrays.asList("Java", "C++", "Lisp", "Haskell").
-      stream().collect(Collectors.<String>toList());
+      stream().filter( it -> it.length() == 4 ).collect(Collectors.<String>toList());
   }
 
   public static void main(String[] args){
     List<String> result = new StreamToListConverter().parse();
-    assert 4 == result.size();
+    assert 2 == result.size();
     assert result.contains("Java");
+    assert result.contains("Lisp");
   }
 
 }
