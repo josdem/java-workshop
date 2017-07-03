f5e5e76955cc8dcaea95752f8b8ef093112315d5
josdem <joseluis.delacruz@gmail.com>
2017-07-03 13:38:52 -0500

    Got reduce example

---
+++ streams/examples/StreamReductor.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.function.Function;
+
+class StreamReductor {
+
+  private Optional<String> getLargestName(List<String> nicknames){
+    return nicknames.stream().reduce((s1,s2) -> s1.length() > s2.length() ? s1 : s2);
+  }
+
+  public static void main(String[] args){
+    List<String> nicknames = Arrays.asList("josdem","trip","erich","martinvilegas","skuarch");
+    Optional<String> result = new StreamReductor().getLargestName(nicknames);
+    assert "martinvilegas" == result.get();
+  }
+
+}
