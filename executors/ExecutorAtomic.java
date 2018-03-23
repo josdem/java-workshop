976a60a3dcba76eca0f24263946a523e251df5b9
josdem <joseluis.delacruz@gmail.com>
2018-03-23 09:21:54 -0600

    Adding atomic example

---
+++ executors/ExecutorAtomic.java
@@ -0,0 +1,26 @@
+import java.util.stream.IntStream;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ExecutorAtomic {
+
+	private final static Integer MAX_PERIOD_TIME = 30;
+	private AtomicInteger atomic = new AtomicInteger(0);
+	private ExecutorService executor = Executors.newFixedThreadPool(3);
+
+	private Integer start() throws InterruptedException {
+		IntStream.range(0, 3).forEach(i -> executor.submit(atomic::incrementAndGet));
+		executor.shutdown();
+
+		executor.awaitTermination(MAX_PERIOD_TIME, TimeUnit.SECONDS);
+		return atomic.get();
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		Integer result = new ExecutorAtomic().start();
+		System.out.println("I have been counting: " + result + " times");
+	}
+
+}
