f2b5806fe88fb770593ede6adc72e1ad9d91fab1
josdem <joseluis.delacruz@gmail.com>
2018-03-22 21:26:11 -0600

    Refactoring executor callable name

--- executors/ExecutorCallable.java
+++ executors/ExecutorCallable.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ExecutionException;
 
-public class ExecutorCallableCounter {
+public class ExecutorCallable {
 
   private final static Integer MAX_PERIOD_TIME = 30;
 
