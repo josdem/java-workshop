29be33ea2024623e5ebf070949f6a1292ab3a12d
josdem <joseluis.delacruz@gmail.com>
2018-04-29 12:24:08 -0500

    Adding functional interfaces function andThen and Function.identity()

--- functional-interfaces/src/test/java/com/jos/dem/functional/FunctionTest.java
+++ functional-interfaces/src/test/java/com/jos/dem/functional/FunctionTest.java
@@ -1,5 +1,6 @@
 package com.jos.dem.functional;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.function.Function;
@@ -12,6 +13,20 @@ public class FunctionTest {
 		Integer expectedResult = 6;
 		Function<String, Integer> function = string -> string.length();
 		assertEquals(expectedResult, function.apply("josdem"));
+	}
+
+	@Test
+	public void shouldKnowIfNicknameLengthIsEven(){
+		Function<String, Integer> lenghtFunction = string -> string.length();
+		Function<Integer, Boolean> evenFunction = integer -> integer % 2 == 0;
+
+		assertTrue(lenghtFunction.andThen(evenFunction).apply("josdem"));
+	}
+
+	@Test
+	public void shouldGetFunctionIdentity(){
+		Function<String, String> function = Function.identity();
+		assertEquals("josdem", function.apply("josdem"));
 	}
 	
 }
