f07bd77f13daf207217a815888bc007dccfce9a9
josdem <joseluis.delacruz@gmail.com>
2018-08-06 19:39:29 -0400

    Adding hello world interface and implementation

---
+++ from-anonymous-to-lambda/Greetings.java
@@ -0,0 +1,5 @@
+public interface Greetings {
+
+  String sayHello();
+
+}
---
+++ from-anonymous-to-lambda/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld implements Greetings {
+
+  public String sayHello(){
+    return "Hello World!";
+  }
+
+}
+
