d9dccd6dc8e6a51ed0690550a8138cff2526b764
josdem <joseluis.delacruz@gmail.com>
2018-09-08 16:02:35 -0400

    Adding display name annotation to the functonal examples

--- functional-interfaces/src/test/java/com/jos/dem/functional/FunctionTest.java
+++ functional-interfaces/src/test/java/com/jos/dem/functional/FunctionTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.function.Function;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class FunctionTest {
 
 	@Test
+  @DisplayName("Understands how to get string lenght")
 	public void shouldGetStringLenght(){
 		Integer expectedResult = 6;
 		Function<String, Integer> function = string -> string.length();
@@ -16,6 +18,7 @@ public class FunctionTest {
 	}
 
 	@Test
+  @DisplayName("Understands how to get josdem lenght and if it is even")
 	public void shouldKnowIfNicknameLengthIsEven(){
 		Function<String, Integer> lenghtFunction = string -> string.length();
 		Function<Integer, Boolean> evenFunction = integer -> integer % 2 == 0;
@@ -24,9 +27,10 @@ public class FunctionTest {
 	}
 
 	@Test
+  @DisplayName("Understands how to get function identity")
 	public void shouldGetFunctionIdentity(){
 		Function<String, String> function = Function.identity();
 		assertEquals("josdem", function.apply("josdem"));
 	}
-	
+
 }
