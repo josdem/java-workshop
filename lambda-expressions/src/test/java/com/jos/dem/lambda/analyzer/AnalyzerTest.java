05909b8fdcfa4cb8cf01f02af42d8e6932b086a4
josdem <joseluis.delacruz@gmail.com>
2018-09-15 12:08:24 -0400

    Adding description name to the analyzer test

--- lambda-expressions/src/test/java/com/jos/dem/lambda/analyzer/AnalyzerTest.java
+++ lambda-expressions/src/test/java/com/jos/dem/lambda/analyzer/AnalyzerTest.java
@@ -3,22 +3,26 @@ package com.jos.dem.lambda.analyzer;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class AnalyzerTest {
 
 	@Test
+  @DisplayName("Understands how to validate a keyword is present")
 	public void shouldKnowContainKeyword(){
 		StringAnalyzer analyzer = new ContainsAnalyzer();
 		assertTrue(analyzer.analyze("In the end, it's not the years in your life that count. It's the life in your years", "life"));
 	}
 
 	@Test
+  @DisplayName("Understands how to validate a keyword is present using lambda expressions")
 	public void shouldKnowContainKeywordUsingLambda(){
 		StringAnalyzer analyzer = (text, keyword) -> text.contains(keyword);
 		assertTrue(analyzer.analyze("In the end, it's not the years in your life that count. It's the life in your years", "life"));
 	}
 
 	@Test
+  @DisplayName("Understands how to validate a keyword is present and ends with using lambda expressions")
 	public void shouldAnalyzeUsingContainsAndEndsWith(){
 		StringAnalyzer analyzerContains = (text, keyword) -> text.contains(keyword);
 		StringAnalyzer analyzerEndsWith = (text, keyword) -> text.endsWith(keyword);
