6664407887e0dc243b21c900d3eb1b016ad23f9f
josdem <joseluis.delacruz@gmail.com>
2018-09-14 20:53:16 -0400

    Adding description name to the supplier test

--- functional-interfaces/src/test/java/com/jos/dem/functional/SupplierTest.java
+++ functional-interfaces/src/test/java/com/jos/dem/functional/SupplierTest.java
@@ -4,13 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import java.util.function.Supplier;
+import org.junit.jupiter.api.DisplayName;
 
 public class SupplierTest {
 
 	@Test
+  @DisplayName("Understands how to get a string using supplier")
 	public void shouldSupplyAnString(){
 		Supplier<String> supplier = () -> "josdem";
 		assertEquals("josdem", supplier.get());
 	}
-	
+
 }
