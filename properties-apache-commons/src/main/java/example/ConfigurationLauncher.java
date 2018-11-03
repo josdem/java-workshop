bac9e02b7fbfa9e5b9753f2edb6349e158b1e9b0
josdem <joseluis.delacruz@gmail.com>
2018-11-03 18:29:44 -0400

    Refactoring to better naming

--- properties-apache-commons/src/main/java/example/ConfigurationLauncher.java
+++ properties-apache-commons/src/main/java/example/ConfigurationLauncher.java
@@ -9,14 +9,13 @@ import org.apache.commons.configuration2.ex.ConfigurationException;
 public class ConfigurationLauncher {
 
   private void readProperties(){
-    Configurations configs = new Configurations();
-    try{ 
-      Configuration config = configs.properties(new File("configuration.properties"));
-      String username = config.getString("username");
-      String password = config.getString("password");
+    Configurations configurations = new Configurations();
+    try{
+      Configuration configuration = configurations.properties(new File("configuration.properties"));
+      String username = configuration.getString("username");
+      String password = configuration.getString("password");
       System.out.println("{username:" + username + ",password:"+ password + "}");
-    }
-    catch (ConfigurationException cex) {
+    } catch (ConfigurationException cex) {
       System.out.println("Error: " + cex.getMessage());
     }
   }
