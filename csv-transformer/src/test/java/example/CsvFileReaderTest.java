40eca4ac6aa066fb0df40c5a564a4a2c4658b443
josdem <joseluis.delacruz@gmail.com>
2019-12-21 09:09:26 -0500

    Validating we have rows

--- csv-transformer/src/test/java/example/CsvFileReaderTest.java
+++ csv-transformer/src/test/java/example/CsvFileReaderTest.java
@@ -1,14 +1,26 @@
 package example;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 class CsvFileReaderTest {
 
+  private CsvFileReader reader = new CsvFileReader();
+
   @Test
-  void shouldSayHello(){
-    assertTrue(true);
+  @DisplayName("Should read csv file")
+  void shouldReadCsvFile(){
+    String path = "src/test/resources/csv.txt";
+    List<List<String>> result = reader.read(path);
+
+    assertAll("rows",
+        () -> assertEquals(4, result.size(), "should have four rows")
+        );
   }
 
 
