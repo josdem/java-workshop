15b878f99ac31838635ad74c004e1e16faeb6526
josdem <joseluis.delacruz@gmail.com>
2020-02-01 13:04:52 -0600

    Adding test should take action if present

--- optional/src/test/java/com/jos/dem/optional/OptionalTest.java
+++ optional/src/test/java/com/jos/dem/optional/OptionalTest.java
@@ -1,7 +1,10 @@
 package com.jos.dem.optonal;
 
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.logging.Logger;
 
 import java.util.Optional;
 
@@ -10,11 +13,13 @@ import org.junit.jupiter.api.Test;
 
 class OptionalTest {
 
+  private static final Logger log = Logger.getLogger(OptionalTest.class.getName());
+
   @Test
   @DisplayName("should validate if is present")
   void shouldBePresent(){
     Optional<String> opt = Optional.ofNullable("josdem");
-    assertTrue(opt.isPresent(), "should not be present");
+    assertTrue(opt.isPresent(), "should be present");
   }
 
   @Test
@@ -24,5 +29,11 @@ class OptionalTest {
     assertFalse(opt.isPresent(), "should not be present");
   }
 
+  @Test
+  @DisplayName("should take action if is present")
+  void shouldPrintValue(){
+    Optional<String> opt = Optional.ofNullable("josdem");
+    opt.ifPresent(value -> log.info("value: " + opt.get()));
+  }
 
 }
