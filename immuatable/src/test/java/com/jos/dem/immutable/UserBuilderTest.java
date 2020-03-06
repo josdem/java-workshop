57626a0a66cff703f2b698666a15a5371e7123bd
josdem <joseluis.delacruz@gmail.com>
2020-03-06 14:52:26 -0500

    Adding user as active

--- immuatable/src/test/java/com/jos/dem/immutable/UserBuilderTest.java
+++ immuatable/src/test/java/com/jos/dem/immutable/UserBuilderTest.java
@@ -1,5 +1,7 @@
 package com.jos.dem.immutable;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +17,19 @@ class UserBuilderTest {
 
         assertEquals("josdem@email.com", user.getEmail(), "should have email");
         assertEquals("password", user.getPassword(), "should have password");
+        assertFalse(user.isActive(), "should not be active");
+    }
+
+    @Test
+    @DisplayName("should create an active user")
+    void shouldCreateActiveUser() {
+        User user = new User.Builder("josdem@email.com", "password")
+            .active(true)
+            .build();
+
+        assertEquals("josdem@email.com", user.getEmail(), "should have email");
+        assertEquals("password", user.getPassword(), "should have password");
+        assertTrue(user.isActive(), "should be active");
     }
 
 }
