916067424a87e3df5572649fb0add8d15a941c64
josdem <joseluis.delacruz@gmail.com>
2020-04-30 19:06:34 -0400

    Formatting code

--- functional-interfaces/src/test/java/com/jos/dem/functional/FunctionTest.java
+++ functional-interfaces/src/test/java/com/jos/dem/functional/FunctionTest.java
@@ -1,61 +1,64 @@
 package com.jos.dem.functional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FunctionTest {
 
-	private List<Person> persons = Arrays.asList(new Person("josdem", 5), new Person("tgrip", 4),
-			new Person("edzero", 3), new Person("jeduan", 5), new Person("siedrix", 5));
+  private final List<Person> persons =
+      Arrays.asList(
+          new Person("josdem", 5),
+          new Person("tgrip", 4),
+          new Person("edzero", 3),
+          new Person("jeduan", 5),
+          new Person("siedrix", 5));
 
-	@Test
-	@DisplayName("Understands how to get string lenght")
-	public void shouldGetStringLenght() {
-		Integer expectedResult = 6;
-		Function<String, Integer> function = string -> string.length();
-		assertEquals(expectedResult, function.apply("josdem"));
-	}
+  @Test
+  @DisplayName("Understands how to get string lenght")
+  public void shouldGetStringLenght() {
+    Integer expectedResult = 6;
+    Function<String, Integer> function = string -> string.length();
+    assertEquals(expectedResult, function.apply("josdem"));
+  }
 
-	@Test
-	@DisplayName("Understands how to get josdem lenght and if it is even")
-	public void shouldKnowIfNicknameLengthIsEven() {
-		Function<String, Integer> lenghtFunction = string -> string.length();
-		Function<Integer, Boolean> evenFunction = integer -> integer % 2 == 0;
+  @Test
+  @DisplayName("Understands how to get josdem lenght and if it is even")
+  public void shouldKnowIfNicknameLengthIsEven() {
+    Function<String, Integer> lenghtFunction = string -> string.length();
+    Function<Integer, Boolean> evenFunction = integer -> integer % 2 == 0;
 
-		assertTrue(lenghtFunction.andThen(evenFunction).apply("josdem"));
-	}
+    assertTrue(lenghtFunction.andThen(evenFunction).apply("josdem"));
+  }
 
-	@Test
-	@DisplayName("Understands how to get function identity")
-	public void shouldGetFunctionIdentity() {
-		Function<String, String> function = Function.identity();
-		assertEquals("josdem", function.apply("josdem"));
-	}
+  @Test
+  @DisplayName("Understands how to get function identity")
+  public void shouldGetFunctionIdentity() {
+    Function<String, String> function = Function.identity();
+    assertEquals("josdem", function.apply("josdem"));
+  }
 
-	@Test
-	@DisplayName("should filter by high ranked and starts with J")
-	void shouldFilterByHighRankedAndLetter(){
-		List<String> expectedPersons = Arrays.asList("josdem", "jeduan");
-		Function<Person, String> nicknames = (p) -> p.getNickname(); 
+  @Test
+  @DisplayName("should filter by high ranked and starts with J")
+  void shouldFilterByHighRankedAndLetter() {
+    List<String> expectedPersons = Arrays.asList("josdem", "jeduan");
+    Function<Person, String> nicknames = (p) -> p.getNickname();
 
-		List<String> names = persons.stream()
-			.filter(p -> p.getRanking() > 4)
-			.map(nicknames)
-			.filter(n -> n.startsWith("j"))
-			.collect(Collectors.toList());
+    List<String> names =
+        persons.stream()
+            .filter(p -> p.getRanking() > 4)
+            .map(nicknames)
+            .filter(n -> n.startsWith("j"))
+            .collect(Collectors.toList());
 
-		assertEquals(2, names.size(), "should have two names");
-		assertEquals(expectedPersons, names, "should have expected filtered persons");
-
-	}
-
+    assertEquals(2, names.size(), "should have two names");
+    assertEquals(expectedPersons, names, "should have expected filtered persons");
+  }
 }
--- functional-interfaces/src/test/java/com/jos/dem/functional/SupplierTest.java
+++ functional-interfaces/src/test/java/com/jos/dem/functional/SupplierTest.java
@@ -1,18 +1,18 @@
 package com.jos.dem.functional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
-import java.util.function.Supplier;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SupplierTest {
 
-	@Test
+  @Test
   @DisplayName("Understands how to get a string using supplier")
-	public void shouldSupplyAnString(){
-		Supplier<String> supplier = () -> "josdem";
-		assertEquals("josdem", supplier.get());
-	}
-
+  public void shouldSupplyAnString() {
+    Supplier<String> supplier = () -> "josdem";
+    assertEquals("josdem", supplier.get());
+  }
 }
