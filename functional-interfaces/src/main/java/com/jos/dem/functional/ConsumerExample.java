b69015e13d3a6fcd339604a3bbc20ca66c4f2230
josdem <joseluis.delacruz@gmail.com>
2020-05-01 19:41:06 -0400

    Formatting code

---
+++ functional-interfaces/src/main/java/com/jos/dem/functional/ConsumerExample.java
@@ -0,0 +1,11 @@
+package com.jos.dem.functional;
+
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+  public static void main(String[] args) {
+    Consumer<String> consumer = x -> System.out.println(x.toLowerCase());
+    consumer.accept("JOSDEM");
+  }
+}
