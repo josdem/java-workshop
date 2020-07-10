1a9e2f1c7508816e47b4a80cefcf22e984609921
josdem <joseluis.delacruz@gmail.com>
2020-07-10 12:52:53 -0400

    Formatting code

--- date-and-time/DateOperations.java
+++ date-and-time/DateOperations.java
@@ -3,18 +3,21 @@ import java.time.Month;
 
 public class DateOperations {
 
-  private void operations(){
-    LocalDate today = LocalDate.now();
-    System.out.println("today in YYYY-MM-DD format: " + today);
-    System.out.println("Day of the week: " + today.getDayOfWeek());
-    System.out.println("Next week: " + today.plusDays(7));
-    System.out.println("Is a Leap Year?: " + today.isLeapYear());
+  private void operations() {
+    LocalDate localDate = LocalDate.now();
+    System.out.println("localDate in YYYY-MM-DD format: " + localDate);
+    System.out.println("Day of the week: " + localDate.getDayOfWeek());
+    System.out.println("Next week: " + localDate.plusDays(7));
+    System.out.println("Is a Leap Year?: " + localDate.isLeapYear());
 
     LocalDate christmas = LocalDate.of(2016, Month.DECEMBER, 25);
-    System.out.println("Today is before Christmas?: " + today.isBefore(christmas));
+    System.out.println("Today is before Christmas?: " + localDate.isBefore(christmas));
+
+    LocalDate christmasParsed = LocalDate.parse("2020-12-25");
+    System.out.println("Christmas Parsed: " + christmasParsed);
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     new DateOperations().operations();
   }
 }
