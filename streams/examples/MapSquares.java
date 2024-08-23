fda3d0dfbcc31fdf6593f7e0ce4e5ab75696a320
josdem <joseluis.delacruz@gmail.com>
2024-08-23 17:46:57 -0400

    Updating Java code

--- streams/examples/MapSquares.java
+++ streams/examples/MapSquares.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 public class MapSquares {
 
   private List<Integer> square(List<Integer> numbers){
-    return numbers.stream().map(it -> it * it).collect(Collectors.toList());
+    return numbers.stream().map(it -> it * it).toList();
   }
 
   public static void main(String[] args){
-    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8);
-    List<Integer> result = new MapSquares().square(numbers);
+    var numbers = List.of(1,2,3,4,5,6,7,8);
+    var result = new MapSquares().square(numbers);
     assert result.size() == 8;
     assert result.get(1) == 4;
     assert result.get(3) == 16;
