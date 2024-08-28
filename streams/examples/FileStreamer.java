039c82607d63517580ce4518431846a89aef3e1a
josdem <joseluis.delacruz@gmail.com>
2024-08-28 09:43:32 -0400

    Deleting extra character

--- streams/examples/FileStreamer.java
+++ streams/examples/FileStreamer.java
@@ -10,7 +10,7 @@ public class FileStreamer {
 	private List<String> read(Path path) throws IOException {
     return Files.lines(path).toList();
 	}
-	
+  
   public static void main(String[] args) throws IOException {
   	var path = Paths.get("../resources/nicknames.txt");
     var result = new FileStreamer().read(path);
