b7f1c858563a04c96f6d70e0637c525b088282fd
josdem <joseluis.delacruz@gmail.com>
2024-08-28 09:36:54 -0400

    Updating Java code

--- streams/examples/FileStreamer.java
+++ streams/examples/FileStreamer.java
@@ -8,12 +8,12 @@ import java.util.stream.Collectors;
 public class FileStreamer {
 
 	private List<String> read(Path path) throws IOException {
-    return Files.lines(path).collect(Collectors.toList());
+    return Files.lines(path).toList();
 	}
 	
   public static void main(String[] args) throws IOException {
-  	Path path = Paths.get("../resources/nicknames.txt");
-    List<String> result = new FileStreamer().read(path);
+  	var path = Paths.get("../resources/nicknames.txt");
+    var result = new FileStreamer().read(path);
     assert result.size() == 6;
     assert result.contains("josdem");
   }
