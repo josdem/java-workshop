6467f8bc5d76da3ed4700eaead87d03109ae8209
josdem <joseluis.delacruz@gmail.com>
2024-08-29 10:34:25 -0400

    Updating Java syntax

--- streams/filters/StringFilter.java
+++ streams/filters/StringFilter.java
@@ -6,12 +6,11 @@ public class StringFilter {
 
   private List<String> parse(){
     return Arrays.asList("Java", "C++", "Lisp", "Haskell").
-      stream().filter( p -> p.startsWith("J")).
-      collect(Collectors.toList());
+      stream().filter( p -> p.startsWith("J")).toList();
   }
 
   public static void main(String[] args){
-    List<String> result = new StringFilter().parse();
+    var result = new StringFilter().parse();
     assert 1 == result.size();
     assert "Java" == result.get(0);
   }
