fcdcd3f461e862083a48986cc19b88e8938468d9
josdem <joseluis.delacruz@gmail.com>
2024-08-29 14:05:10 -0400

    Adding missing identation

--- streams/examples/FileStreamer.java
+++ streams/examples/FileStreamer.java
@@ -7,12 +7,12 @@ import java.util.stream.Collectors;
 
 public class FileStreamer {
 
-	private List<String> read(Path path) throws IOException {
+  private List<String> read(Path path) throws IOException {
     return Files.lines(path).toList();
 	}
   
   public static void main(String[] args) throws IOException {
-  	var path = Paths.get("../resources/nicknames.txt");
+    var path = Paths.get("../resources/nicknames.txt");
     var result = new FileStreamer().read(path);
     assert result.size() == 6;
     assert result.contains("josdem");
