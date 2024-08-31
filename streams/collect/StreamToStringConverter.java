8cc22ac4307b600a492744df791961e1eaa18b70
josdem <joseluis.delacruz@gmail.com>
2024-08-31 14:32:22 -0400

    Updating Java syntax

--- streams/collect/StreamToStringConverter.java
+++ streams/collect/StreamToStringConverter.java
@@ -1,16 +1,15 @@
 import java.util.List;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class StreamToStringConverter {
 
   private String parse(){
-    return Arrays.asList("Java", "C++", "Lisp", "Haskell").
+    return List.of("Java", "C++", "Lisp", "Haskell").
       stream().collect(Collectors.joining(","));
   }
 
   public static void main(String[] args){
-    String result = new StreamToStringConverter().parse();
+    var result = new StreamToStringConverter().parse();
     assert "Java,C++,Lisp,Haskell".equals(result);
   }
 
