d4201be93fb12a3a077968a1b53f558271449596
josdem <joseluis.delacruz@gmail.com>
2024-09-02 12:31:07 -0400

    Updating Java syntax

--- streams/collect/StreamToMapConverter.java
+++ streams/collect/StreamToMapConverter.java
@@ -1,17 +1,17 @@
 import java.util.Map;
-import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamToMapConverter {
 
   private Map<String, Integer> parse(){
-    return Arrays.asList("Java", "C++", "Lisp", "Haskell").
+    return List.of("Java", "C++", "Lisp", "Haskell").
       stream().collect(Collectors.toMap(Function.identity(), String::length));
   }
 
   public static void main(String[] args){
-    Map<String, Integer> result = new StreamToMapConverter().parse();
+    var result = new StreamToMapConverter().parse();
     assert 4 == result.size();
     assert result.get("Java") == 4;
     assert result.get("C++") == 3;
