13885361e8255d81eeb0cd3793a48172eb0fb624
josdem <joseluis.delacruz@gmail.com>
2024-09-03 10:18:09 -0400

    Adding missing identation

--- streams/collect/GroupByCollector.java
+++ streams/collect/GroupByCollector.java
@@ -10,9 +10,9 @@ public class GroupByCollector {
 
   public static void main(String[] args){
   	var persons = List.of(
-		new Person("josdem", RoleType.DEVELOPER),
-      	new Person("tgtip", RoleType.DEVELOPER),
-      	new Person("erich", RoleType.TESTER)
+      new Person("josdem", RoleType.DEVELOPER),
+      new Person("tgtip", RoleType.DEVELOPER),
+      new Person("erich", RoleType.TESTER)
     );
     var result = new GroupByCollector().parse(persons);
     assert result.get(RoleType.DEVELOPER).size() == 2;
