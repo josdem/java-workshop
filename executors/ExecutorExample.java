b273e63c956c695b94870ef06b5925dbba51c108
josdem <joseluis.delacruz@gmail.com>
2024-09-05 13:29:24 -0400

    Simplyfing code

--- executors/ExecutorExample.java
+++ executors/ExecutorExample.java
@@ -4,22 +4,17 @@ import java.util.concurrent.TimeUnit;
 
 public class ExecutorExample {
 
-  private static final Integer MAX_PERIOD_TIME = 30;
+  private static final Integer MAX_PERIOD_TIME = 10;
 
   private ExecutorService executor = Executors.newSingleThreadExecutor();
 
   private void start() throws InterruptedException {
-    executor.execute(this::task);
+    executor.execute(task);
     executor.shutdown();
     executor.awaitTermination(MAX_PERIOD_TIME, TimeUnit.SECONDS);
   }
 
-  private Runnable task() {
-
-    return () -> {
-      System.out.println("Asynchronous task");
-    };
-  }
+  private Runnable task = () -> System.out.println("Asynchronous task");
 
   public static void main(String[] args) throws InterruptedException {
     new ExecutorExample().start();
