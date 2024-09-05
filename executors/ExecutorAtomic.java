2e9b93252b102fa47539fc3eb229489c75203caa
josdem <joseluis.delacruz@gmail.com>
2024-09-05 15:05:12 -0400

    Improving grammar

--- executors/ExecutorAtomic.java
+++ executors/ExecutorAtomic.java
@@ -6,20 +6,19 @@ import java.util.stream.IntStream;
 
 public class ExecutorAtomic {
 
-  private static final Integer MAX_PERIOD_TIME = 30;
+  private static final Integer MAX_PERIOD_TIME = 5;
   private AtomicInteger atomic = new AtomicInteger(0);
   private ExecutorService executor = Executors.newFixedThreadPool(3);
 
   private Integer start() throws InterruptedException {
     IntStream.range(0, 3).forEach(i -> executor.execute(atomic::incrementAndGet));
     executor.shutdown();
-
     executor.awaitTermination(MAX_PERIOD_TIME, TimeUnit.SECONDS);
     return atomic.get();
   }
 
   public static void main(String[] args) throws InterruptedException {
-    Integer result = new ExecutorAtomic().start();
+    var result = new ExecutorAtomic().start();
     assert result == 3;
   }
 }
