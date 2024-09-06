0bc881ef802f369dbb31cea8ac015e6d54faf4e4
josdem <joseluis.delacruz@gmail.com>
2024-09-06 15:41:35 -0400

    Updating Java syntax

--- executors/ExecutorCallable.java
+++ executors/ExecutorCallable.java
@@ -1,14 +1,18 @@
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorCallable {
 
-  private static final Integer MAX_PERIOD_TIME = 30;
+  private static final Integer MAX_PERIOD_TIME = 10;
 
   private ExecutorService executor = Executors.newFixedThreadPool(3);
 
   private Integer start() throws InterruptedException, ExecutionException {
-    Future<Integer> future = executor.submit(new CallableThread());
-    final Integer result = future.get();
+    var future = executor.submit(new CallableThread());
+    final var result = future.get();
     executor.shutdown();
 
     executor.awaitTermination(MAX_PERIOD_TIME, TimeUnit.SECONDS);
@@ -16,7 +20,7 @@ public class ExecutorCallable {
   }
 
   public static void main(String[] args) throws InterruptedException, ExecutionException {
-    Integer result = new ExecutorCallable().start();
+    var result = new ExecutorCallable().start();
     assert result == 3;
   }
 }
@@ -25,7 +29,7 @@ class CallableThread implements Callable<Integer> {
 
   @Override
   public Integer call() throws InterruptedException {
-    final Integer wait = 3;
+    final var wait = 3;
     TimeUnit.SECONDS.sleep(wait);
     return wait;
   }
