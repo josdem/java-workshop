a58e587b35ac452bd072053ed2e1aa2fb8a267ff
josdem <joseluis.delacruz@gmail.com>
2024-09-12 15:12:48 -0400

    Updating Java syntax

--- executors/CompletableFutureRunAsync.java
+++ executors/CompletableFutureRunAsync.java
@@ -4,20 +4,19 @@ import java.util.concurrent.TimeUnit;
 
 public class CompletableFutureRunAsync {
 
-  private void start() throws InterruptedException, ExecutionException {
-    CompletableFuture<Void> completableFuture =
-        CompletableFuture.runAsync(
-            () -> {
-              try {
-                TimeUnit.SECONDS.sleep(3);
-              } catch (InterruptedException ie) {
-                ie.printStackTrace();
-              }
-            });
-    completableFuture.get();
-  }
+  private final static int SLEEP_TIME = 3;
+
+  private CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> {
+    try {
+      TimeUnit.SECONDS.sleep(SLEEP_TIME);
+    } catch (InterruptedException ie) {
+      ie.printStackTrace();
+    }
+  });
 
   public static void main(String[] args) throws InterruptedException, ExecutionException {
-    new CompletableFutureRunAsync().start();
+    var future = new CompletableFutureRunAsync().completableFuture;
+    future.join();
+    assert future.isDone();
   }
 }
