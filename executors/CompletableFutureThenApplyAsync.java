c3c34bb98609272ca6d5a125887b0b91347253a5
josdem <joseluis.delacruz@gmail.com>
2024-09-13 12:19:51 -0400

    Updating Java syntax

--- executors/CompletableFutureThenApplyAsync.java
+++ executors/CompletableFutureThenApplyAsync.java
@@ -6,15 +6,14 @@ public class CompletableFutureThenApplyAsync {
 
   private String start() throws InterruptedException, ExecutionException {
     final Supplier<String> supplier = () -> "hello";
-    CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(supplier);
-
-    CompletableFuture<String> future = completableFuture.thenApplyAsync(value -> value + " world");
+    final CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(supplier);
+    final CompletableFuture<String> future = completableFuture.thenApplyAsync(value -> value + " world");
 
     return future.get();
   }
 
   public static void main(String[] args) throws InterruptedException, ExecutionException {
-    String result = new CompletableFutureThenApplyAsync().start();
+    var result = new CompletableFutureThenApplyAsync().start();
     assert result.equals("hello world");
   }
 }
