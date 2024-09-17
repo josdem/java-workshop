3cab1ee5e6c31faa760f9eb878a1b7e482fdc5e3
josdem <joseluis.delacruz@gmail.com>
2024-09-17 12:14:13 -0400

    Updating Java syntax

--- generics/ListPrinter.java
+++ generics/ListPrinter.java
@@ -1,25 +1,22 @@
 import java.util.List;
 import java.util.Arrays;
-
 public class ListPrinter {
 
   private <T> void printList(List<T> collection){
     for(T item: collection){
       System.out.printf("%s ", item);
     }
-    System.out.println();
   }
 
   public static void main(String[] args){
-    List<Integer> integers = Arrays.asList(1,2,3,4,5);
-    List<Double> doubles = Arrays.asList(1.1,2.2,3.3,4.4,5.5);
-    List<Character> string = Arrays.asList('J','O','S','D','E','M');
-    ListPrinter printer = new ListPrinter();
-    System.out.println("Printing integers");
+    var integers = List.of(1,2,3,4,5);
+    var doubles = List.of(1.1,2.2,3.3,4.4,5.5);
+    var chars = List.of('J','O','S','D','E','M');
+    var printer = new ListPrinter();
     printer.printList(integers);
-    System.out.println("Printing doubles");
+    System.out.println();
     printer.printList(doubles);
-    System.out.println("Printing string");
-    printer.printList(string);
+    System.out.println();
+    printer.printList(chars);
   }
 }
