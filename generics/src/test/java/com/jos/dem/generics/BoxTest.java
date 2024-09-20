ad43839a1ad8999c1365a4f53d7c69895439359a
josdem <joseluis.delacruz@gmail.com>
2024-09-20 15:31:51 -0400

    Updating Java syntax

--- generics/src/test/java/com/jos/dem/generics/BoxTest.java
+++ generics/src/test/java/com/jos/dem/generics/BoxTest.java
@@ -9,18 +9,18 @@ class BoxTest {
 
   @Test
   @DisplayName("should contains a string in the box")
-  void shouldConstainsAStringInTheBox(){
+  void shouldContainsAStringInTheBox(){
     String nickname = "josdem";    
-    Box<String> box = new Box<String>();
+    var box = new Box<String>();
     box.set(nickname);
     assertEquals(nickname, box.get());
   }
 
   @Test
-  @DisplayName("shold contains an integer in the box")
+  @DisplayName("should contains an integer in the box")
   void shouldContainsAnIntegerInTheBox(){
-    Box<Integer> box = new Box<Integer>();   
-    box.set(Integer.valueOf(10));
+    var box = new Box<Integer>();
+    box.set(10);
     assertEquals(10, box.get());
   }
 
