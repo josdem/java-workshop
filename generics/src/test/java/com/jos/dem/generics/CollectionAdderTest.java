c92ac3bde43938b0af73614e916755bacf093c8d
josdem <joseluis.delacruz@gmail.com>
2024-09-23 11:28:03 -0400

    Updating Java syntax

--- generics/src/test/java/com/jos/dem/generics/CollectionAdderTest.java
+++ generics/src/test/java/com/jos/dem/generics/CollectionAdderTest.java
@@ -1,30 +1,28 @@
 package com.jos.dem.generics;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
-import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CollectionAdderTest {
 
-  private CollectionAdder collectionAdder = new CollectionAdder();
+    private final CollectionAdder collectionAdder = new CollectionAdder();
 
-  @Test
-  @DisplayName("should sum integers")
-  void shouldSumIntegers(){
-    List<Integer> numbers = Arrays.asList(Integer.valueOf(10), Integer.valueOf(11), Integer.valueOf(12));
-    assertEquals(Double.valueOf(33), collectionAdder.sum(numbers));
-  }
+    @Test
+    @DisplayName("should sum integers")
+    void shouldSumIntegers() {
+        var numbers = List.of(10, 11, 12);
+        assertEquals(Double.valueOf(33), collectionAdder.sum(numbers));
+    }
 
-  @Test
-  @DisplayName("should sum doubles")
-  void shouldSumDoubles(){
-    List<Double> numbers = Arrays.asList(Double.valueOf(10), Double.valueOf(11), Double.valueOf(12));
-    assertEquals(Double.valueOf(33), collectionAdder.sum(numbers));
-  }
+    @Test
+    @DisplayName("should sum doubles")
+    void shouldSumDoubles() {
+        List<Double> numbers = List.of(10.5, 11.2, 12.8);
+        assertEquals(Double.valueOf(34.5), collectionAdder.sum(numbers));
+    }
 
 }
